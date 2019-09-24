701989e7ae18859d1549ea528575c2da3f629a4f
Rissancha <53909318+Rissancha@users.noreply.github.com>
2019-09-24 22:09:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
-import redis
-import os
 import telebot
-# import some_api_lib
-# import ...
-
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
-
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
-
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+bot = telebot.TeleBot('916099039:AAFYJZr93eHu0LuUHDfbFLWQO-kK_1uy-28')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет,ублюдок,пока что ты можешь посмотреть расписание,просто введи "расписание"')
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'расписание':
+        bot.send_message(message.chat.id, 'Понедельник : Математика,Захист,Физика\nВторник: География,История,Информатика\nСреда: Физкультура,Английский язык,Украинский язык\nЧетверг: Економика,История,Физика\nПятница: Биолигия,Украинская лит-ра,Зар. лит-ра')
+    elif message.text == 'Расписание':
+        bot.send_message(message.chat.id, 'Понедельник : Математика,Захист,Физика\nВторник: География,История,Информатика\nСреда: Физкультура,Английский язык,Украинский язык\nЧетверг: Економика,История,Физика\nПятница: Биолигия,Украинская лит-ра,Зар. лит-ра")')
+    else :
+        bot.send_message(message.chat.id, 'Сам понял что высрал?(пока что доступна только команда "расписание")')
+bot.polling()
